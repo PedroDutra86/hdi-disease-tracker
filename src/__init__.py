60f4907fad5630993ba4cf83e3e69021058aca8b
Pedro Dutra <pedrodutra86@hotmail.com>
2025-03-22 19:04:03 -0300

    chore: add gitignore and requirements files and updating __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,3 @@
-from .data_loader import load_data
-from .data_cleaner import clean_data
-from .analysis import analyze_data
+from data_loader import load_data
+from data_cleaner import clean_data
+from data_selector import select_data
